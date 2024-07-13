1b0810be9481712a305715db71e6ceb2d64fcd7f
Dr-dyrane <halodyrane@gmail.com>
2024-07-13 10:18:41 +0100

    subheading fix

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -23,7 +23,7 @@ const HeroSection = () => {
 					initial={{ y: 50, opacity: 0 }}
 					animate={{ y: 0, opacity: 1 }}
 					transition={{ duration: 1, delay: 0.5 }}
-					className="mt-4 text-contrast/85 dark:text-contrast-dark/80 text-left leading-7 sm:leading-8 md:leading-9 lg:leading-10 xl:leading-loose 2xl:leading-relaxed md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl"
+					className="mt-4 text-contrast/85 dark:text-contrast-dark/80 text-left leading-7 sm:leading-8 md:leading-9 lg:leading-10 xl:leading-loose 2xl:leading-relaxed text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl"
 				>
 					Streamline processes, boost productivity, and drive growth with our
 					all-in-one business tool. Enhance your brand, analyze performance, and
